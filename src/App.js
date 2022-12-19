12ffc5afa76f8599751cc661f127b21608948230
graslowsnail <graslowsnail@gmail.,com>
2022-12-19 14:32:41 -0600

    added about me section and picture, and nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import './App.css';
+import React from 'react';
+import Nav from './componets/Nav';
+import About from './componets/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+      <Nav />
+        <main>
+            <About />
+        </main>
+      </div>
+      );
 }
 
 export default App;
