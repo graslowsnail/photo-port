d5c8c4d2ef2071377878e9452caad523801626b4
graslowsnail <graslowsnail@gmail.,com>
2023-01-01 14:23:46 -0600

    test for contact from

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -22,5 +22,5 @@ describe('Gallery is rendering', () => {
         const { getByTestId } = render(<Gallery currentCategory={portrait} />)
         expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
     })
-
+        
 })
